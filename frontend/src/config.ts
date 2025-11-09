6da7089927e5ec44468b5172fd26f0636966925d
Çağatay Kalan <ckalan@gmail.com>
2025-11-09 14:59:05 +0300

    Implement header navigation and pricing page layout; add site configuration

---
+++ frontend/src/config.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "ReportViz",
+  company: "Bivektor Inc.",
+  contact: {
+    address: "169 Madison Ave STE 38162<br/>New York, NY 10016",
+  },
+  links: {
+    privacy: "/privacy-policy",
+    terms: "/terms-of-service",
+    pricing: "/pricing",
+  },
+  paymentLinks: {
+    sdk: "https://buy.stripe.com/test_14k14g4lBeCOfU0aEE",
+    basicSupport: "https://buy.stripe.com/test_14k14g4lBeCOfU0aEE",
+    premiumSupport: "https://buy.stripe.com/test_14k14g4lBeCOfU0aEE",
+  },
+
+
+} as const;
