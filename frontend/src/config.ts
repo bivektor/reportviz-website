d2798dfa6814b06aa6dfc9c372dbb4fe8c1d948c
Çağatay Kalan <ckalan@gmail.com>
2025-11-09 16:08:48 +0300

    Refactor pricing components to use dynamic Stripe payment links; enhance pricing page layout and add support package details

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -9,11 +9,28 @@ export const siteConfig = {
     terms: "/terms-of-service",
     pricing: "/pricing",
   },
-  paymentLinks: {
-    sdk: "https://buy.stripe.com/test_14k14g4lBeCOfU0aEE",
-    basicSupport: "https://buy.stripe.com/test_14k14g4lBeCOfU0aEE",
-    premiumSupport: "https://buy.stripe.com/test_14k14g4lBeCOfU0aEE",
-  },
-
-
 } as const;
+
+interface StripeConfig {
+  sdkPaymentLink: string;
+  supportTableId: string;
+  supportTablePublishingKey: string;
+}
+
+export function getStripeConfig(): StripeConfig {
+  return stripeConfig.live;
+}
+
+const stripeConfig = {
+  test: {
+      sdkPaymentLink: "https://buy.stripe.com/test_14k14g4fZ9dQ0gU6oo",
+      supportTableId: "prctbl_1SRXmHFZadBQIVy9V8o8gd3f",
+      supportTablePublishingKey: "pk_test_51RvK1wFZadBQIVy992V1glw005tG4GesJFlsGrF3YFZqFW8gc3WXuY1Prl31hn9s1pybCG1wt1vZXhowaMKzS5m300QTOhrC0T",
+    },
+
+    live: {
+      sdkPaymentLink: "https://buy.stripe.com/3cIcN5ci502C1UPc2mbfO00",
+      supportTableId: "prctbl_1SRY2jFZadBQIVy97dTysADC",
+      supportTablePublishingKey: "pk_live_51RvK1wFZadBQIVy9Qt8LiV3Z2dpz9HytZi6JPrkFGLSd2FVrPaMQCLHyRDffPrdoAaxmuawzXCH3D9xAudJlpfvT00ulNJzt5k",
+    }
+}
